240f13b0b4d0ff027e6a960c2245632ac7a61da5
Robert Reinhard <robert.reinhard@bukwild.com>
2023-09-21 15:40:31 -0700

    Export ObjectFitOption from @react-visual/sanity-next

--- packages/sanity-next/src/index.ts
+++ packages/sanity-next/src/index.ts
@@ -1,4 +1,5 @@
 import SanityNextVisual from './SanityNextVisual'
 export default SanityNextVisual
 export { SanityNextVisualProps } from './types/sanityNextVisualTypes'
+export { ObjectFitOption } from '@react-visual/next'
 export { makeImageUrl, makeImageBuilder, makeFileUrl } from './lib/urlBuilding'
