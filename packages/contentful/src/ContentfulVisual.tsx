67436dd9279d3350bd9df780bff96753f3c901d4
Robert Reinhard <robert.reinhard@bukwild.com>
2023-11-29 22:21:16 -0800

    Move logic into lib files

--- packages/contentful/src/ContentfulVisual.tsx
+++ packages/contentful/src/ContentfulVisual.tsx
@@ -1,7 +1,18 @@
 import type { ReactElement } from 'react'
 import ReactVisual from '@react-visual/react'
-import { ContentfulAsset, ContentfulImageAsset, ContentfulVisualEntry, ContentfulVisualProps } from './types/contentfulVisualTypes'
-import type { AspectCalculator, ImageLoader, VideoLoader } from '@react-visual/react'
+import { ContentfulAsset, ContentfulVisualProps } from './types/contentfulVisualTypes'
+import {
+  contentfulModernFormats,
+  defaultImageLoader,
+  defaultVideoLoader
+} from './lib/urlBuilding'
+import {
+  orientationMediaQueries,
+  getImageAspect,
+  responsiveAspectCalculator,
+  hasResponsiveAssets,
+  hasResponsiveAspects
+} from './lib/aspectRatio'
 
 // Render a Visual using Contentful data
 export default function ContentfulVisual(
@@ -48,78 +59,6 @@ export default function ContentfulVisual(
     />
   )
 }
-
-// Produce Contentful image URls
-const defaultImageLoader: ImageLoader = ({ src, width, type, media }) => {
-
-  // Use portrait image if it exists, otherwise fallback to landscape
-  const url = media?.includes('portrait') && src.portraitImage ?
-    src.portraitImage.url :
-    src.image?.url || src.url
-
-  // Make Contentful resizing instructions
-  const query = new URLSearchParams({ w: String(width) })
-
-  // Optionally set the format
-  if (type?.includes('avif')) query.set('fm', 'avif')
-  else if (type?.includes('webp')) query.set('fm', 'webp')
-
-  // Make the query with formatting instructions
-  return `${url}?${query}`
-}
-
-// Use portrait video from `src` prop or fallback to url
-const defaultVideoLoader: VideoLoader = ({ src, media }) => {
-  return media?.includes('portrait') && src.portraitVideo ?
-    src.portraitVideo.url :
-    src.video?.url || src.url
-}
-
-// Get the aspect ratio from an image asset if it exists
-function getImageAspect(
-  image: ContentfulImageAsset | undefined
-): number | undefined {
-  if (!image) return undefined
-  return image.width / image.height
-}
-
-// Make the aspect ratio for responsive assets.
-const responsiveAspectCalculator: AspectCalculator = (
-  { media, image: src }
-) => {
-  if (media?.includes('portrait')) {
-    return src.portraitImage.width / src.portraitImage.height as number
-  } else {
-    return src.image.width / src.image.height as number
-  }
-}
-
-// The types that Contentful's CDN can produce
-const contentfulModernFormats = ['image/avif', 'image/webp']
-
-// Check whether multiple orientations were provided
-function hasResponsiveAssets(src: ContentfulVisualEntry | undefined): boolean {
-  if (!src) return false
-  const hasLandscape = !!(src.image || src.video),
-    hasPortrait = !!(src.portraitImage || src.portraitVideo)
-  return hasLandscape && hasPortrait
-}
-
-// Check whether multiple aspect ratios were provided
-function hasResponsiveAspects(src: ContentfulVisualEntry | undefined): boolean {
-  if (!src) return false
-  const hasLandscapeAspect = !!(src.image?.width &&
-      src.image?.height),
-    hasPortraitAspect = !!(src.portraitImage?.width &&
-      src.portraitImage?.height)
-  return hasLandscapeAspect && hasPortraitAspect
-}
-
-// The media queries that are used for default responsive visuals
-const orientationMediaQueries = [
-  '(orientation:landscape)',
-  '(orientation:portrait)',
-]
 
 // Use various asset fields to make the alt from automatically
 function makeAssetAlt(asset: ContentfulAsset | undefined): string {
---
+++ packages/contentful/src/lib/aspectRatio.ts
@@ -0,0 +1,49 @@
+import type { ContentfulImageAsset, ContentfulVisualEntry } from '../types/contentfulVisualTypes'
+import type { AspectCalculator } from '@react-visual/react'
+
+// The media queries that are used for default responsive visuals
+export const orientationMediaQueries = [
+  '(orientation:landscape)',
+  '(orientation:portrait)',
+]
+
+// Get the aspect ratio from an image asset if it exists
+export function getImageAspect(
+  image: ContentfulImageAsset | undefined
+): number | undefined {
+  if (!image) return undefined
+  return image.width / image.height
+}
+
+// Make the aspect ratio for responsive assets.
+export const responsiveAspectCalculator: AspectCalculator = (
+  { media, image: src }
+) => {
+  if (media?.includes('portrait')) {
+    return src.portraitImage.width / src.portraitImage.height as number
+  } else {
+    return src.image.width / src.image.height as number
+  }
+}
+
+// Check whether multiple orientations were provided
+export function hasResponsiveAssets(
+  src: ContentfulVisualEntry | undefined
+): boolean {
+  if (!src) return false
+  const hasLandscape = !!(src.image || src.video),
+    hasPortrait = !!(src.portraitImage || src.portraitVideo)
+  return hasLandscape && hasPortrait
+}
+
+// Check whether multiple aspect ratios were provided
+export function hasResponsiveAspects(
+  src: ContentfulVisualEntry | undefined
+): boolean {
+  if (!src) return false
+  const hasLandscapeAspect = !!(src.image?.width &&
+      src.image?.height),
+    hasPortraitAspect = !!(src.portraitImage?.width &&
+      src.portraitImage?.height)
+  return hasLandscapeAspect && hasPortraitAspect
+}
