3381353c74b0ba916b1cf31e16417018085d8b8b
Robert Reinhard <robert.reinhard@goodness.inc>
2025-03-25 13:03:02 -0700

    Remove width/height from fillStyles
    When combined with a forced `relative` on the parent, it can cause the visual to expand outside of container

--- packages/react/src/lib/styles.ts
+++ packages/react/src/lib/styles.ts
@@ -3,8 +3,6 @@ import type { CSSProperties } from 'react'
 // Mimic the styles that next/image sets when `fill` prop is applied
 export const fillStyles = {
   position: 'absolute',
-  height: '100%',
-  width: '100%',
   inset: '0px',
 } as CSSProperties
 
