b5cbd5a099ee52953daba03ee91a34b84315deba
Robert Reinhard <robert.reinhard@goodness.inc>
2026-03-03 08:41:15 -0800

    Add Netlify friendly demo build process

--- packages/react/vite.demo.config.ts
+++ packages/react/vite.demo.config.ts
@@ -5,8 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "demo",
+  base: "./",
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
   },
 });
